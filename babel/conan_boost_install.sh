b4d96591a507b9a8df66508bfa6ecadb4e961840
JakaResse <jeremy.bouton@epitech.eu>
2018-09-20 21:20:30 +0200

    babel cmake test on windows

---
+++ babel/conan_boost_install.sh
@@ -0,0 +1,3 @@
+sudo pip install conan
+sudo pip3 install conan
+sudo apt install libboost-all-dev
---
+++ babel/gui_src/main.cpp
@@ -0,0 +1,17 @@
+//
+// EPITECH PROJECT, 2018
+// main gui
+// File description:
+// temporary main
+//
+
+#include <cstdio>
+#include <boost/asio.hpp>
+
+
+int main()
+{
+	boost::asio::io_service io_service;
+	printf("gui cmake ready");
+	return 0;
+}
